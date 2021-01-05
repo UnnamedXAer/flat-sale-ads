9bb6da53857f6aa7c1df187593ee484ea67d471c
UnnamedXAer <UnnamedXAer@gmail.com>
2021-01-05 12:56:20 +0100

    Update jsdoc

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,6 +1,11 @@
-export const sleep = (timeout: number) =>
+/**
+ * `sleep` waits for given time
+ *
+ * @param {number} timeout - time in `ms`
+ */
+export const sleep = (timeoutMs: number) =>
 	new Promise((resolve) =>
 		setTimeout(() => {
 			resolve(void 0);
-		}, timeout)
+		}, timeoutMs)
 	);
