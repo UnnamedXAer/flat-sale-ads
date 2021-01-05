61e78a836ea8550bef7b88472229a820a25c0c32
UnnamedXAer <UnnamedXAer@gmail.com>
2021-01-05 12:56:05 +0100

    Update time formating

--- src/performance.ts
+++ src/performance.ts
@@ -17,14 +17,18 @@ import l from './logger';
  * @param {string} label
  * @returns {Function} timerStop - function to stop the timer and log it with label
  */
-export const timeStart = (label?: string): Function => {
+export const timeStart = (label: string = ''): Function => {
 	const startTime = process.hrtime.bigint();
-	return () => {
-		l.debug(
-			label,
-			`- execution time: ${
-				Number(process.hrtime.bigint() - startTime) / 1000000
-			} ms`
-		);
+	return (stopLabel: string = label) => {
+		let executionTime = Number(process.hrtime.bigint() - startTime);
+		let executionTimeUnit = 'ns';
+		if (executionTime > 100000000) {
+			executionTime /= 1000000000;
+			executionTimeUnit = 's';
+		} else if (executionTime > 100000) {
+			executionTime /= 1000000;
+			executionTimeUnit = 'ms';
+		}
+		l.debug(stopLabel, `- execution time: ${executionTime} ${executionTimeUnit}`);
 	};
 };
