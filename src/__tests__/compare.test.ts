83b6a406baa5b8d6260ec9ff76183434d427860e
UnnamedXAer <unnamedxaer.git@gmail.com>
2021-11-17 21:45:40 +0100

    improve test

--- src/__tests__/compare.test.ts
+++ src/__tests__/compare.test.ts
@@ -11,7 +11,7 @@ const json = `[{
 	},
 	"description": "",
 	"dt": "16 listopada 2021, 15:44",
-	"offerId": "717499681",
+	"offerId": "71749968",
 	"imgUrl": "https://ireland.apollo.olxcdn.com:443/v1/files/u6zmf6m8trop-PL/image;s=644x461",
 	"price": "294000",
 	"scrapedAt": {
@@ -21,8 +21,26 @@ const json = `[{
 	},
 	"site": "olx",
 	"title": "Wyjątkowy Taras Na Dachu, 2 Pokoje"
-},
-{
+},{
+	"url": "https://www.otodom.pl/pl/oferta/-1.wyjatkowy-taras-na-dachu-2-pokoje-ID4bUcv.html?",
+	"_dt": {
+		"$date": {
+			"$numberLong": "1637073840000"
+		}
+	},
+	"description": "",
+	"dt": "16 listopada 2021, 15:44",
+	"offerId": "71749967",
+	"imgUrl": "https://ireland.apollo.olxcdn.com:443/v1/files/u6zmf6m8trop-PL/image;s=644x461",
+	"price": "294000",
+	"scrapedAt": {
+		"$date": {
+			"$numberLong": "1637096204824"
+		}
+	},
+	"site": "olx",
+	"title": "-1Wyjątkowy Taras Na Dachu, 2 Pokoje"
+},{
 	"url": "https://www.otodom.pl/pl/oferta/wyjatkowy-taras-na-dachu-2-pokoje-ID4bUcv.html?",
 	"_dt": {
 		"$date": {
@@ -31,7 +49,7 @@ const json = `[{
 	},
 	"description": "",
 	"dt": "16 listopada 2021, 15:43",
-	"offerId": "717499682",
+	"offerId": "71749968",
 	"imgUrl": "https://ireland.apollo.olxcdn.com:443/v1/files/92ctl2fm308r1-PL/image;s=644x461",
 	"price": "294000",
 	"scrapedAt": {
@@ -41,18 +59,77 @@ const json = `[{
 	},
 	"site": "olx",
 	"title": "Wyjątkowy Taras Na Dachu, 2 Pokoje"
+},{
+	"url": "https://www.otodom.pl/pl/oferta/2.wyjatkowy-taras-na-dachu-2-pokoje-ID4bUcv.html?",
+	"_dt": {
+		"$date": {
+			"$numberLong": "1637073780000"
+		}
+	},
+	"description": "",
+	"dt": "16 listopada 2021, 15:43",
+	"offerId": "717499682",
+	"imgUrl": "https://ireland.apollo.olxcdn.com:443/v1/files/92ctl2fm308r1-PL/image;s=644x461",
+	"price": "294990",
+	"scrapedAt": {
+		"$date": {
+			"$numberLong": "1637096204824"
+		}
+	},
+	"site": "olx",
+	"title": "2. Wyjątkowy Taras Na Dachu, 2 Pokoje"
+},{
+	"url": "https://www.otodom.pl/pl/oferta/2.wyjatkowy-taras-na-dachu-2-pokoje-ID4bUcv.html?",
+	"_dt": {
+		"$date": {
+			"$numberLong": "1637073780000"
+		}
+	},
+	"description": "",
+	"dt": "16 listopada 2021, 15:43",
+	"offerId": "717499682",
+	"imgUrl": "https://ireland.apollo.olxcdn.com:443/v1/files/92ctl2fm308r1-PL/image;s=644x461",
+	"price": "294990",
+	"scrapedAt": {
+		"$date": {
+			"$numberLong": "1637096204824"
+		}
+	},
+	"site": "olx",
+	"title": "2. Wyjątkowy Taras Na Dachu, 2 Pokoje"
+},{
+	"url": "https://www.otodom.pl/pl/oferta/3.wyjatkowy-taras-na-dachu-2-pokoje-ID4bUcv.html?",
+	"_dt": {
+		"$date": {
+			"$numberLong": "1637073780000"
+		}
+	},
+	"description": "",
+	"dt": "16 listopada 2021, 15:43",
+	"offerId": "717499683",
+	"imgUrl": "https://ireland.apollo.olxcdn.com:443/v1/files/92ctl2fm308r1-PL/image;s=644x461",
+	"price": "294993",
+	"scrapedAt": {
+		"$date": {
+			"$numberLong": "1637096204824"
+		}
+	},
+	"site": "olx",
+	"title": "3. Wyjątkowy Taras Na Dachu, 2 Pokoje"
 }]`;
 
 const data: IOffer[] = JSON.parse(json);
 
 describe('compare', () => {
 	it('assert equal offer', () => {
-		const areSame = assertSameOffers(data[0], data[1]);
+		const areSame = assertSameOffers(data[1], data[2]);
 		expect(areSame).toBe(true);
 	});
 
 	it.only('filter out recurred offers', async () => {
-		const unique = await filterOutRecurredOffers(data.reverse(), data);
-		expect(unique).toHaveLength(0);
+		const prevOffers = data.reverse().slice(0, 2);
+		const newOffers = data;
+		const unique = await filterOutRecurredOffers(prevOffers, newOffers);
+		expect(unique).toHaveLength(2);
 	});
 });
